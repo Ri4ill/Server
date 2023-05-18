dd3a026f6bf27923dcbc17ad74c823458bfba072
Ri4ill <r.safurik@gmail.com>
2023-05-18 18:58:16 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express()
+const http = require('http')
+const cors = require('cors');
+const {Server} = require('socket.io')
+const PORT  =  process.env.PORT || 3001
+app.use(cors());//прослойка между нач и концом 
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin:'http://localhost:3000',
+        methods: ["GET", "POST"]
+    }
+})
+//connection мы слушаем событие с id 
+io.on("connection", (socket)=> {
+    console.log(`${socket.id} User ok`);
+    socket.on("join_room", (data)=>{
+        socket.join(data)
+        console.log(`User with ID: ${socket.id} join eoom ${data}`)
+    })
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("resive_message", data)
+        console.log(data)
+    })
+    socket.on("disconnect", ()=>{
+        console.log("user live", socket.io)
+    })
+})
+
+server.listen(PORT,(err)=>{
+    if(err){
+        console.log(err)
+    }
+    console.log("hi let start code")
+})
+
